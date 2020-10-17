7783ee6a36598054aa4ca544a308ca6586f7c3d9
sachin <sachinsagar561@gmail.com>
2020-10-17 18:41:55 +0530

    html travel website desgin

---
+++ static/demo.py
@@ -0,0 +1,9 @@
+
+num = input('Enter you number :')
+
+if num in range(1,11) :
+    print('number is less than or equal to 10')
+elif num in range (11,51):
+    print('number is in range of 10- 50')
+else:
+    print('Number is higher than 50')
